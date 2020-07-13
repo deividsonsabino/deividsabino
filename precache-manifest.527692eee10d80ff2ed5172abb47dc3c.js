e8595be34cf4e1863d625f23d99a650a6b81997a
deivid sabino <xdeividsabinox@live.com>
2020-07-12 22:15:07 -0300

    Updates

---
+++ precache-manifest.527692eee10d80ff2ed5172abb47dc3c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c2669d1a15a884b459fdbedd89688a",
+    "url": "/blk-design-system-react/index.html"
+  },
+  {
+    "revision": "611979ffb2d4c7ad3948",
+    "url": "/blk-design-system-react/static/css/main.2ab6e3dc.chunk.css"
+  },
+  {
+    "revision": "4ddd86147661eefe93c7",
+    "url": "/blk-design-system-react/static/js/2.4f66b311.chunk.js"
+  },
+  {
+    "revision": "f93d91195706ef96b5ed492769112adb",
+    "url": "/blk-design-system-react/static/js/2.4f66b311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611979ffb2d4c7ad3948",
+    "url": "/blk-design-system-react/static/js/main.f433e9ea.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/blk-design-system-react/static/js/main.f433e9ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73acff2820c117cd9cf",
+    "url": "/blk-design-system-react/static/js/runtime-main.2cfc2012.js"
+  },
+  {
+    "revision": "d6e3cb3e8d7afc07a2aac0e47b6f457a",
+    "url": "/blk-design-system-react/static/media/deivid-331.d6e3cb3e.png"
+  },
+  {
+    "revision": "8624a0c3fcc20ed347df2b3a883ed704",
+    "url": "/blk-design-system-react/static/media/deividsabinogames-01.8624a0c3.png"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/blk-design-system-react/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "fec4a3abc8d696d46b8bf4af47c8b379",
+    "url": "/blk-design-system-react/static/media/dslogo.fec4a3ab.png"
+  },
+  {
+    "revision": "c006294c3226b1e45281d891cd2e1f16",
+    "url": "/blk-design-system-react/static/media/jogandocast-01.c006294c.png"
+  },
+  {
+    "revision": "caea7969c56edd572d96ee37cd3e1df1",
+    "url": "/blk-design-system-react/static/media/jogandocast-02.caea7969.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/blk-design-system-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/blk-design-system-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/blk-design-system-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/blk-design-system-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/blk-design-system-react/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "d64994156800b134424fe77d631bef24",
+    "url": "/blk-design-system-react/static/media/skate-01.d6499415.jpg"
+  },
+  {
+    "revision": "e5a9826cf29075703624916033a1507d",
+    "url": "/blk-design-system-react/static/media/skate-02.e5a9826c.jpg"
+  },
+  {
+    "revision": "30c130c0f4e4d3f11025c2f4f4a3a4db",
+    "url": "/blk-design-system-react/static/media/skate-03.30c130c0.jpg"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/blk-design-system-react/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/blk-design-system-react/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/blk-design-system-react/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/blk-design-system-react/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/blk-design-system-react/static/media/square5.ff004a0e.png"
+  }
+]);
